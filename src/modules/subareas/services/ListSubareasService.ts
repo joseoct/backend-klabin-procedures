2873fe11804f5b8c1a20768e460d34729438f1c5
Jose <joseoctavioconti@gmail.com>
2020-06-22 20:01:00 -0300

    Name of service changed

---
+++ src/modules/subareas/services/ListSubareasService.ts
@@ -0,0 +1,20 @@
+import { inject, injectable } from 'tsyringe';
+
+import Subarea from '../infra/typeorm/entities/Subarea';
+import ISubareasRepository from '../repositories/ISubareasRepository';
+
+@injectable()
+class ListSubareasService {
+  constructor(
+    @inject('SubareasRepository')
+    private subareasRepository: ISubareasRepository,
+  ) {}
+
+  public async execute(): Promise<Subarea[]> {
+    const subareas = await this.subareasRepository.list();
+
+    return subareas;
+  }
+}
+
+export default ListSubareasService;
